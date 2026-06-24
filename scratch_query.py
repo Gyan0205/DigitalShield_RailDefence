17ee9765f9a648673e7c6889564c0c6021a18613
YELLAPU GYAN SAGAR <gyansagar.ssc.9a.2020@gmail.com>
2026-06-24 13:44:04 +0530

    Delete scratch_query.py

--- scratch_query.py
+++
@@ -1,7 +0,0 @@
-from backend.database import db_service
-from sqlalchemy import text
-
-with db_service.engine.connect() as conn:
-    res = conn.execute(text("SELECT * FROM trains WHERE platform_number=3 AND departure_days LIKE '%Wed%'")).fetchall()
-    for row in res:
-        print(dict(row._mapping))
